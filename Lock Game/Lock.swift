ded872e98c7c56e9b6089744b7136ef5e3022093
Blake <russeth.blake@gmail.com>
2023-08-14 22:02:07 -0700

    -

---
+++ Lock Game/Lock.swift
@@ -0,0 +1,52 @@
+//
+//  Lock.swift
+//  Lock Game
+//
+//  Created by Blake Russeth on 8/13/23.
+//
+
+import SpriteKit
+
+class LockTop: SKSpriteNode {
+    
+    init() {
+        let texture = SKTexture(imageNamed: "Lock_Top")
+        
+        super.init(texture:texture, color:.white, size: texture.size())
+        
+        name = "lock_top"
+        position = CGPoint(x: 0, y: 200)
+        setScale(1.5)
+        anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        zPosition = -1
+        
+        
+        
+    }
+    required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+}
+
+class LockBase: SKNode {
+    let base: SKShapeNode!
+    override init() {
+        base = SKShapeNode(circleOfRadius: 200)
+                base.strokeColor = .gray
+                base.fillColor = SKColor(red: 38.0/255.0, green: 38.0/255.0, blue: 38.0/255.0, alpha: 1.0)
+                base.lineWidth = 30
+                
+                super.init()
+                
+                position = CGPoint(x: 0, y: 0)
+                zPosition = 0
+                
+                addChild(base)
+        }
+    required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+}
+    
+    
+
